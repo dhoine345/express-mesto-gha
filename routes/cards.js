85e05445e05bfd2a2f8db8d334915f169d773a7b
dhoine345 <lue42@yandex.ru>
2022-06-22 17:06:49 +0300

    fix some bugs

--- routes/cards.js
+++ routes/cards.js
@@ -1,4 +1,5 @@
 const router = require('express').Router();
+const { celebrate, Joi } = require('celebrate');
 const auth = require('../middlewares/auth');
 const {
   createCard,
@@ -8,7 +9,12 @@ const {
   removeLike,
 } = require('../controllers/cards');
 
-router.post('/', auth, createCard);
+router.post('/', celebrate({
+  body: Joi.object().keys({
+    name: Joi.string().required().min(2).max(30),
+    link: Joi.string().required().link(),
+  }),
+}), auth, createCard);
 router.get('/', auth, getCards);
 router.delete('/:cardId', auth, deleteCard);
 router.put('/:cardId/likes', auth, setLike);
