55ee6143013265e7fad1648817ac634ebb58958e
dhoine345 <lue42@yandex.ru>
2022-06-22 17:14:00 +0300

    fix some bugs

--- routes/cards.js
+++ routes/cards.js
@@ -12,7 +12,7 @@ const {
 router.post('/', celebrate({
   body: Joi.object().keys({
     name: Joi.string().required().min(2).max(30),
-    link: Joi.string().required().link(),
+    //link: Joi.string().required().link(),
   }),
 }), auth, createCard);
 router.get('/', auth, getCards);
