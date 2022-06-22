295f5408e422b27a1d7d079b250359b7fe5c089e
dhoine345 <lue42@yandex.ru>
2022-06-22 13:23:22 +0300

    fix: no hash in res

--- app.js
+++ app.js
@@ -12,14 +12,6 @@ mongoose.connect('mongodb://localhost:27017/mestodb', {
   useUnifiedTopology: true,
 });
 
-/*app.use((req, res, next) => {
-  req.user = {
-    _id: '62a2ffd2ed7f327aef731968',
-  };
-
-  next();
-});*/
-
 app.use(express.json());
 app.use(express.urlencoded({
   extended: true,
