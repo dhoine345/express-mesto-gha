0a99d81dd796626671368a0665a1a493fb390095
dhoine345 <lue42@yandex.ru>
2022-06-23 09:19:51 +0300

    fix auth

--- routes/cards.js
+++ routes/cards.js
@@ -1,6 +1,5 @@
 const router = require('express').Router();
 const { celebrate, Joi } = require('celebrate');
-const auth = require('../middlewares/auth');
 const {
   createCard,
   getCards,
@@ -9,14 +8,14 @@ const {
   removeLike,
 } = require('../controllers/cards');
 
-router.post('/', auth, celebrate({
+router.post('/', celebrate({
   body: Joi.object().keys({
     name: Joi.string().required().min(2).max(30),
   }).unknown(true),
 }), createCard);
-router.get('/', auth, getCards);
-router.delete('/:cardId', auth, deleteCard);
-router.put('/:cardId/likes', auth, setLike);
-router.delete('/:cardId/likes', auth, removeLike);
+router.get('/', getCards);
+router.delete('/:cardId', deleteCard);
+router.put('/:cardId/likes', setLike);
+router.delete('/:cardId/likes', removeLike);
 
 module.exports = router;
