dae0cfd86be2b469342c3cb74087fe3394acec99
dhoine345 <lue42@yandex.ru>
2022-06-28 21:23:57 +0300

    test

--- controllers/users.js
+++ controllers/users.js
@@ -3,6 +3,8 @@ const jwt = require('jsonwebtoken');
 const User = require('../models/user');
 const { handleErrors, handleRequest } = require('../utils/utils');
 const { resCodes, errorMessages, randomString } = require('../utils/constants');
+const BadRequestError = require('../utils/errors/NotFoundError');
+const ConflictError = require('../utils/errors/ConflictError');
 
 const createUser = (req, res, next) => {
   const {
@@ -16,7 +18,14 @@ const createUser = (req, res, next) => {
     .then((user) => res.status(resCodes.CREATED_CODE).send({
       name: user.name, about: user.about, avatar: user.avatar, email: user.email, _id: user._id,
     }))
-    .catch((err) => handleErrors(err, next));
+    .catch((error) => {
+      if (error.name === 'ValidationError') {
+        next(new BadRequestError(errorMessages.badRequest));
+      } else if (error.code === 11000) {
+        next(new ConflictError(errorMessages.emailError));
+      }
+      next(error);
+    });
 };
 
 const login = (req, res) => {
@@ -28,7 +37,6 @@ const login = (req, res) => {
       res.send({ token });
     })
     .catch((err) => {
-      console.log('errinlogin', err.message);
       res.status(401).send({ message: err.message });
     });
 };
