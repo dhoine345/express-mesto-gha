22a4f75877e105c3136e2c589f19651c0f89fdc7
dhoine345 <lue42@yandex.ru>
2022-06-29 10:15:51 +0300

    fix some bugs

--- controllers/cards.js
+++ controllers/cards.js
@@ -2,6 +2,7 @@ const Card = require('../models/card');
 const { handleErrors, handleRequest } = require('../utils/utils');
 const { resCodes, errorMessages } = require('../utils/constants');
 const ForbiddenError = require('../utils/errors/ForbiddenError');
+const NotFoundError = require('../utils/errors/NotFoundError');
 
 const createCard = (req, res, next) => {
   const { name, link } = req.body;
@@ -22,7 +23,7 @@ const deleteCard = (req, res, next) => {
   Card.findById(req.params.cardId)
     .then((card) => {
       if (!card) {
-        res.status(resCodes.NOT_FOUND_ERROR).send(errorMessages.cardError);
+        throw new NotFoundError(errorMessages.cardError);
       } else if (req.user._id.toString() !== card.owner.toString()) {
         throw new ForbiddenError(errorMessages.forbidden);
       }
